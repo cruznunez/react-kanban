dbbaa1aafcf43b164a3b11e1d78cdfd90fb8ad11
Cruz Nunez <nunez.a.cruz@gmail.com>
2018-01-31 00:09:04 +0000

    Rm publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ const common = merge(
       // E.g., '/kanban-demo/'. Webpack will alter asset paths
       // based on this. You can even use an absolute path here
       // or even point to a CDN.
-      publicPath: '/react-kanban/'
+      // publicPath: '/react-kanban/'
     },
     resolve: {
       extensions: ['', '.js', '.jsx']
